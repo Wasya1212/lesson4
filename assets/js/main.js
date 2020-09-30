9d7b695f90c6ee419de5e0d2900f71ede7e47c55
Wasya1212 <leonardo.1212@yandex.ua>
2020-09-30 17:18:53 +0300

    added margins

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,31 @@ const sliderOpts = {
   arrows: false,
   speed: 2000,
   rewind: true,
-  pagination: false
+  pagination: false,
+  pauseOnHover: false
 };
 
+function checkElementOffset(element) {
+  const windowHeight = window.innerHeight;
+  const elementHeight = element.offsetHeight;
+
+  window.addEventListener('scroll', e => {
+    if (window.pageYOffset > windowHeight * 0.5) {
+      element.classList.add('fixedToTop');
+      document.body.style.marginTop = `${elementHeight}px`;
+    } else {
+      element.classList.remove('fixedToTop');
+      document.body.removeAttribute('style');
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  const headerElement = document.querySelector('.top-header');
+
+  // make header fixed to top
+  // if it is out the window
+  checkElementOffset(headerElement);
+
   const slider = new Splide('.splide', sliderOpts).mount();
 });
