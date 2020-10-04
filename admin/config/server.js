b39bd7f89efa5bf164ac14d01fc5fac746a36f77
Wasya1212 <leonardo.1212@yandex.ua>
2020-10-04 15:52:22 +0300

    added admin

---
+++ admin/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f43948df3e7ab48ac21403066a8b46f'),
+    },
+  },
+});
---
+++ admin/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '834cba16-e8c9-4ce0-a61c-60ed57dc058b'
+};
